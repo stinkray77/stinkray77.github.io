3ccbb2beff496a77f70fc628f18f9b712dac452b
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-10-09 17:41:11 +0800

    feat: use jsx project

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -6,11 +6,7 @@ const ProjectsPage: React.FC = () => {
   return (
     <HomeLayout>
       <div className="pt-12 lg:pt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
-        <ProjectCard project={{ name: "Project 1", description: "project 1" }} />
-        <ProjectCard project={{ name: "Project 2", description: "project 2" }} />
-        <ProjectCard project={{ name: "Project 3", description: "project 3" }} />
-        <ProjectCard project={{ name: "Project 4", description: "project 4" }} />
-        <ProjectCard project={{ name: "Project 5", description: "project 5" }} />
+        {projects.map(project => <ProjectCard key={project.name} project={project} />)}
       </div>
     </HomeLayout>
   );
