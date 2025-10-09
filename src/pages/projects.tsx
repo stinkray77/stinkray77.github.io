d3554c3e7065b50a1b483f899ca7912c040a6c24
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-10-09 17:42:16 +0800

    feat: use jsx projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,12 +1,39 @@
 import React from "react";
 import HomeLayout from "@/components/layout/home-layout";
+import { ProjectItem } from "@/types/projects";
 import ProjectCard from "@/components/ui/project-card";
 
 const ProjectsPage: React.FC = () => {
+  const projects: ProjectItem[] = [
+    {
+      name: "Project 1",
+      description: "Description of project",
+      date: "Aug 2025 - Present",
+      actions: [{
+        text: "Repo",
+        link: "https://github.com/ShearesWeb/ReactWorkSHop/blob/main/src/pages/index.tsx"
+      }]
+    },
+    {
+      name: "Project 2",
+      description: "Description of project",
+      date: "Aug 2025 - Present",
+      actions: [{
+        text: "Repo",
+        link: "https://github.com/ShearesWeb/ReactWorkSHop/blob/main/src/pages/index.tsx"
+      }]
+    }
+  ]
+
   return (
     <HomeLayout>
-      <div className="pt-12 lg:pt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
-        {projects.map(project => <ProjectCard key={project.name} project={project} />)}
+      <div className="max-w-7xl px-4 sm:px-6 lg:px-8 mx-auto">
+        <h2 className="text-xl font-semibold mb-4">Projects Page!</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {projects.map(project => (
+            <ProjectCard project={project}></ProjectCard>
+          ))}
+        </div>
       </div>
     </HomeLayout>
   );
